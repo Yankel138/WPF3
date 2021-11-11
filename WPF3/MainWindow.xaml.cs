ff5cbd57398120da6c6dd36f4aa99e2f1b380537
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-11-11 11:30:24 +0300

    .

--- WPF3/MainWindow.xaml.cs
+++ WPF3/MainWindow.xaml.cs
@@ -35,29 +35,66 @@ namespace WPF3
         }
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
             if (textBox != null)
             {
-                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBox).Text);
                 textBox.FontSize = fontSize;
             }
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            string textColor = (sender as RadioButton).Name;
+            string textColor = (sender as RadioButton).Content as string;
 
-            if (textColor == "Черный")
-                textBox.Foreground = new SolidColorBrush(Colors.Black);
-            else if (textColor == "Красный")
-                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            if (textBox != null)
+            {
+                if (textColor == "Красный")
+                    textBox.Foreground = new SolidColorBrush(Colors.Red);
+                else if (textColor == "Зеленый")
+                    textBox.Foreground = new SolidColorBrush(Colors.Green);
+                else
+                    textBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
         }
 
-        private void RadioButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        private void Button_Click_I(object sender, RoutedEventArgs e)
         {
 
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
         }
 
+        private void Button_Click_B(object sender, RoutedEventArgs e)
+        {
+
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Button_Click_U(object sender, RoutedEventArgs e)
+        {
+
+            if (textBox.TextDecorations == TextDecorations.Underline)
+            {
+                textBox.TextDecorations = new TextDecorationCollection();
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+        }
 
     }
 }
